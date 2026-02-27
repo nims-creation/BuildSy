74ce36d4747d659083014f2a5d3d8fc8375038bc
Nims-Creation <nimscreation06@gmail.com>
2026-02-27 22:05:20 +0530

    Structuring the quary

--- src/main/java/com/nims_creation/projects/BuildSy/Repository/ProjectRepository.java
+++ src/main/java/com/nims_creation/projects/BuildSy/Repository/ProjectRepository.java
@@ -11,21 +11,26 @@ import java.util.Optional;
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     @Query("""
-            SELECT p FROM Project p 
+            SELECT p FROM Project p
             WHERE p.deletedAt IS NULL
             AND EXISTS(
-            SELECT 1 FROM ProjectMember pm
-            WHERE pm.id.userId = :userId
-            AND pm.id.projectId = p.id
+                SELECT 1 FROM ProjectMember pm
+                WHERE pm.id.userId = :userId
+                AND pm.id.projectId = p.id
             )
             ORDERED BY p.updatedAt DESC
-            """)
+           """)
     List<Project> findAllAccessibleByUser(@Param("userId") Long userId);
 
     @Query("""
             SELECT p FROM Project p
             WHERE p.id = :projectId
-            AND p.deletedAt IS NULL
+                AND p.deletedAt IS NULL
+                AND EXISTS(
+                    SELECT 1 FROM ProjectMember pm
+                    WHERE pm.id.userId = :userId
+                    AND pm.id.projectId = p.id
+                )
             """)
     Optional<Project> findAccessibleProjectById(@Param("projectId") Long projectId,
                                                 @Param("userId")Long userId);
