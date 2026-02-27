560fb9386d826cd9c6b108e3d7446ffba9e1a0e4
Nims-Creation <nimscreation06@gmail.com>
2026-02-27 21:45:21 +0530

    updating the quary adding inner joint in the quary

--- src/main/java/com/nims_creation/projects/BuildSy/Repository/ProjectRepository.java
+++ src/main/java/com/nims_creation/projects/BuildSy/Repository/ProjectRepository.java
@@ -13,6 +13,11 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     @Query("""
             SELECT p FROM Project p 
             WHERE p.deletedAt IS NULL
+            AND EXISTS(
+            SELECT 1 FROM ProjectMember pm
+            WHERE pm.id.userId = :userId
+            AND pm.id.projectId = p.id
+            )
             ORDERED BY p.updatedAt DESC
             """)
     List<Project> findAllAccessibleByUser(@Param("userId") Long userId);
