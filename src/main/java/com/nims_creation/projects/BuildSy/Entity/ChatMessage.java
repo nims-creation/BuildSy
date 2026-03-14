9c219893d7c0077a3bd26539e8b87ad2786e9384
Nims-Creation <nimscreation06@gmail.com>
2026-03-14 23:32:38 +0530

    update chatMessage entity

--- src/main/java/com/nims_creation/projects/BuildSy/Entity/ChatMessage.java
+++ src/main/java/com/nims_creation/projects/BuildSy/Entity/ChatMessage.java
@@ -1,26 +1,46 @@
 package com.nims_creation.projects.BuildSy.Entity;
 
 import com.nims_creation.projects.BuildSy.Entity.Enum.MessageRole;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.boot.jaxb.mapping.spi.db.JaxbColumnDefinable;
 
 import java.time.Instant;
 
+@Entity
+@Table(name = "chat_messages")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ChatMessage {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumns({
+            @JoinColumn(name = "project_id", referencedColumnName = "project_id", nullable = false),
+            @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
+    })
     ChatSession chatSession;
 
+
+    @Column(columnDefinition = "text", nullable = false)
     String content;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     MessageRole role;
-    String toolCalls; // JSON Array of Tools Called
-    Integer tokensUsed;
 
+
+    String toolCalls; // JSON Array of Tools Called
+    Integer tokensUsed = 0;
+
+    @CreationTimestamp
     Instant createdAt;
 }
