068ea109de42b24a2713882c3509eb0362a725fc
Nims-Creation <nimscreation06@gmail.com>
2026-03-14 23:45:14 +0530

    modified the chatSession Entity

--- src/main/java/com/nims_creation/projects/BuildSy/Entity/ChatSession.java
+++ src/main/java/com/nims_creation/projects/BuildSy/Entity/ChatSession.java
@@ -1,24 +1,39 @@
 package com.nims_creation.projects.BuildSy.Entity;
 
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Instant;
 
+@Entity
 @Getter
 @Setter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ChatSession {
 
+    @EmbeddedId
+    private ChatSessionId chatSessionId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId("projectId")
+    @JoinColumn(name = "project_id", nullable = false, updatable = false)
     Project project;
 
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId("userId")
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
     User user;
 
-    String title;
-
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
     Instant createdAt;
+
+    @UpdateTimestamp
     Instant updatedAt;
 
     Instant deletedAt; //soft delete
